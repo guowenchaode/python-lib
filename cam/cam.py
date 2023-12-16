3731d238241d1fdc376816858519ce4402fa6fe4
guowenchaode <guowenchaode@126.com>
2023-12-16 18:49:30 +0800

    2023-12-16-18:49:30.05

--- cam/cam.py
+++ cam/cam.py
@@ -9,24 +9,43 @@ import argparse
 import imutils
 import time
 import cv2
-prototxt = r'D:\Git\github\python-lib\MobileNetSSD_deploy.prototxt'
-model = r'D:\Git\github\python-lib\MobileNetSSD_deploy.caffemodel'
+
+prototxt = r"D:\Git\github\python-lib\MobileNetSSD_deploy.prototxt"
+model = r"D:\Git\github\python-lib\MobileNetSSD_deploy.caffemodel"
 confidence = 0.9
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-           "sofa", "train", "tvmonitor"]
+CLASSES = [
+    "background",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 net = cv2.dnn.readNetFromCaffe(prototxt, model)
-vf = 1
-vs = VideoStream(vf).start()
-fps = FPS().start()
-while True:
-    frame = vs.read()
+
+
+def process(frame):
     frame = imutils.resize(frame, width=400)
     (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-                                 0.007843, (300, 300), 127.5)
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5
+    )
     net.setInput(blob)
     detections = net.forward()
     for i in np.arange(0, detections.shape[2]):
@@ -35,22 +54,46 @@ while True:
             idx = int(detections[0, 0, i, 1])
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
-            label = "{}: {:.2f}%".format(CLASSES[idx],
-                                         conf * 100)
-            cv2.rectangle(frame, (startX, startY), (endX, endY),
-                          COLORS[idx], 2)
+            label = "{}: {:.2f}%".format(CLASSES[idx], conf * 100)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[idx], 2)
             y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+            cv2.putText(
+                frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2
+            )
     cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-    fps.update()
-fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+
+
+def open_cam(fps, vs):
+    success = True
+    while True:
+        frame = vs.read()
+        process(frame)
+        key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            success = False
+            break
+        fps.update()
+    fps.stop()
+    print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
+    return success
+
+
+def open():
+    while True:
+        try:
+            vf = 1
+            vs = VideoStream(vf).start()
+            fps = FPS().start()
+            if not open_cam(fps, vs):
+                break
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    open()
